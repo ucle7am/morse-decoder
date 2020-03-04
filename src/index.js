2c2b8e08f295a62283dd2c2a7c8d395fe90514ec
Alexander <59923093+ucle7am@users.noreply.github.com>
2020-03-04 18:26:37 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    for (let a = 0; a < expr.length; a += 10) {
+        arr.push(expr.slice(a, a + 10));
+    }
+
+    /*for (let b = 0; b < arr.length; b++) {
+        if(arr[b] == '**********'){
+            arr.splice(b,1,' ');
+        }   
+    }*/
+    let str = '';
+    let k = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        k = 0;
+        for (k; k < arr[i].length; k+=2) {
+            if(arr[i][k]+arr[i][k+1] == '10') str += '.';
+            else if(arr[i][k]+arr[i][k+1] == '11') str += '-';
+        }
+        arr.splice(i,1,str);
+        str = '';
+    }
+
+    for (let c = 0; c < arr.length; c++) {
+        if(arr[c] !== ''){
+            arr.splice(c,1,MORSE_TABLE[arr[c]]);
+        }    
+    }
+
+    let res = '';
+
+    for (let b = 0; b < arr.length; b++) {
+        if(arr[b] == ''){
+            res+= ' ';
+        }else{
+            res += arr[b];
+        }   
+    }
+    
+    return res;
 }
 
 module.exports = {
